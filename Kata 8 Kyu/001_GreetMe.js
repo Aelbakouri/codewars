0ee96bd72766c7a0097b52f13fa297819691788e
Aelbakouri <allal.elbakourii@gmail.com>
2022-03-27 19:04:29 +0100

    greet me

---
+++ Kata 8 Kyu/001_GreetMe.js
@@ -0,0 +1,9 @@
+// 01: Greet Me
+// example
+//** */ "riley" --> "Hello Riley!"
+//** */ "JACK"  --> "Hello Jack!"
+
+var greet = function(name) {
+return 'Hello ' + name[0].toUpperCase()+name.slice(1).toLowerCase()+ '!'
+};
+console.log(greet('allal'))
