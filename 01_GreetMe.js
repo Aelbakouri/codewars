c16ad5923930743d6a8482246d216970d50fcd25
Aelbakouri <allal.elbakourii@gmail.com>
2022-03-27 23:22:11 +0100

    yes

--- 01_GreetMe.js
+++
@@ -1,9 +0,0 @@
-// 01: Greet Me
-// example
-//** */ "riley" --> "Hello Riley!"
-//** */ "JACK"  --> "Hello Jack!"
-
-// var greet = function(name) {
-// return 'Hello ' + name[0].toUpperCase()+name.slice(1).toLowerCase()+ '!'
-// };
-// console.log(greet('allal'))
