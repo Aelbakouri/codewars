9c835cd61261d2d62e160b18b31e32934b5b5565
Allal EL BAKOURI <95944107+Aelbakouri@users.noreply.github.com>
2022-05-20 23:31:43 +0000

    Update 007_Don't give me five.js

--- Kata 7 Kyu/007_Don't give me five.js
+++ Kata 7 Kyu/007_Don't give me five.js
@@ -9,7 +9,7 @@
 // The start number will always be smaller than the end number. Both numbers can be also negative!
 
 // I'm very curious for your solutions and the way you solve it. Maybe someone of you will find an easy pure mathematics solution.
-
+ 
 // Have fun coding it and please don't forget to vote and rank this kata! :-)
 
 // I have also created other katas. Take a look if you enjoyed this kata!
