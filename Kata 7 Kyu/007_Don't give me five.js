7e31a8202924035f1c6140b4be3666bb6588a407
Allal EL BAKOURI <95944107+Aelbakouri@users.noreply.github.com>
2022-05-20 23:31:09 +0000

    Update 007_Don't give me five.js

--- Kata 7 Kyu/007_Don't give me five.js
+++ Kata 7 Kyu/007_Don't give me five.js
@@ -2,7 +2,7 @@
 
 // Examples:
 
-// 1,9 -> 1,2,3,4,6,7,8,9 -> Result 8
+// 1,9 -> 1,2,3,4,6,7,8,9 -> Result 8 
 // 4,17 -> 4,6,7,8,9,10,11,12,13,14,16,17 -> Result 12
  
 // The result may contain fives. ;-)
